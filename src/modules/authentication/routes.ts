8f3eef2de4ba3f58fead6e4779cb0be720cf29fb
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-02-08 21:18:25 -0500

    added route to verify token

--- src/modules/authentication/routes.ts
+++ src/modules/authentication/routes.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Router } from 'express';
+import passport from '../../middleware/passport';
 import githubAuthController from './githubAuthController';
 import googleAuthController from './googleAuthController';
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post('/github', githubAuthController);
 router.post('/google', googleAuthController);
+router.get('/token', passport.authenticate('jwt'));
 
 export default router;
