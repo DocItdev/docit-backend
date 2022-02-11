6981808c693c9bf7f43f79d006d7c50c16f7fc2a
PaulAlek <pavlo.aleksyeyev@gmail.com>
2022-02-10 22:48:12 -0500

    added get and create for project

--- src/modules/projects/createProjectController.ts
+++ src/modules/projects/createProjectController.ts
@@ -1,7 +1,24 @@
-import {Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes';
-import {getAllProjects, createProject} from '../../services/projectServices';
+import { createProject } from '../../services/projectServices';
 
 export default async function createProjectController(req: Request, res: Response) {
-  
+  try{
+    const { body } = req;
+    const projectName = body.name;
+    const projectDescription = body.description;
+    //@ts-ignore
+    const userId = req.user.id;
+    const project = await createProject({
+        name:projectName,
+        description: projectDescription,
+        userId
+    })
+
+    return res.status(StatusCodes.OK).json(project);
+
+  } catch(error){
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+
+  }
 }
