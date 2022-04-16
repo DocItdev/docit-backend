6c5b1f597aa8dfa5978972f9bf2f87493a45177d
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-04-16 19:44:07 -0400

    removed local streaming functionality

--- src/modules/mediastorage/mediastorage.service.ts
+++ src/modules/mediastorage/mediastorage.service.ts
@@ -1,4 +1,7 @@
-import S3 from 'aws-sdk/clients/s3';
+// import S3 from 'aws-sdk/clients/s3';
+import { S3, GetObjectCommand } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage';
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import s3Config from '../../config/s3Config';
 import { AwsFile, UploadedFile } from './mediastorage.interface';
 
@@ -80,5 +83,5 @@ export async function deleteUploadedFile(file: UploadedFile) {
   await s3Client.deleteObject({
     Bucket: bucketName,
     Key: file.path,
-  }).promise();
+  });
 }
