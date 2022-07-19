b4dc212072657b169fd4cac9143f99550c0813b2
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-07-18 20:44:14 -0400

    made auth token expire time 15 min

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,6 +2,6 @@
 
 export const paramMissingError = 'One or more of the required parameters was missing.';
 
-export const AUTH_TOKEN_EXPIRES_IN = 1000 * 15; // 15m
+export const AUTH_TOKEN_EXPIRES_IN = 1000 * 60 * 15; // 15m
 
 export const REFRESH_TOKEN_EXPIRES_IN = 1000 * 60 * 60 * 12; // 12h
